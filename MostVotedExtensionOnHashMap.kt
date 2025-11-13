c218a05911b698f2ca76fa9c83c3a4c897293d21
Roopi <roopabhadravathi@gmail.com>
2025-11-13 16:51:26 +0530

    highest value holding entry in hash may by calling maxByOrNull takes lamda function key value and retruns value which is not null by type safe call returning key on top of lambda function

---
+++ MostVotedExtensionOnHashMap.kt
@@ -0,0 +1,16 @@
+package com.ust.mykotlineprograms
+
+fun Map<String, Int>.mostVoted(): String? {
+    return maxByOrNull { (key, value) -> value }?.key
+}
+
+fun main() {
+    val poll = mapOf(
+        "Cats" to 37,
+        "Dogs" to 58,
+        "Birds" to 22
+    )
+
+    println("Top choice: ${poll.mostVoted()}") 
+    // Dogs
+}
